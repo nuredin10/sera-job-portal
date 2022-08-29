615b57ddef51b51f7b63de6534f2341951ae41b8
kimot <kimot@DESKTOP-6N9AOMP>
2022-08-29 23:49:05 +0300

    ignore the home controllers

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sera_job_portal_api.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
