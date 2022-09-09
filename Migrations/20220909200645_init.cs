bec0cb42a8cd4cbf7cba58cddc8d9b3997ab63f7
nuredin10 <nuredinibrahim18@gmail.com>
2022-09-09 23:46:38 +0300

    job api added

---
+++ Migrations/20220909200645_init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sera_job_portal_api.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "jobs",
+                columns: table => new
+                {
+                    JobId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JobTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobPrice = table.Column<long>(type: "bigint", nullable: false),
+                    JobDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_jobs", x => x.JobId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "jobs");
+        }
+    }
+}
