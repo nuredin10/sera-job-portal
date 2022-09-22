2a67659ab5bff3ffd288c7eebda9a097d6e38867
kimot <kimot@DESKTOP-6N9AOMP>
2022-09-22 13:42:57 +0300

    model changes

--- Models/Job.cs
+++ Models/Job.cs
@@ -10,14 +10,25 @@ namespace Sera_job_portal_api.Models
     public class Job
     {
         [Key]
+
         public int JobId { get; set; }
+
+        [Required(ErrorMessage = "Title for your job is required")]
         public string JobTitle { get; set; }
+
+        [Required(ErrorMessage = "price is required")]
         public long JobPrice { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string JobDescription { get; set; }
+
+        [Required(ErrorMessage = "job Type is required is required")]
         public string JobType { get; set; }
+
+        
         public string CompanyName { get; set; }
         public DateTime PostedDate { get; set; }
         public string Location { get; set; }
-        //public int EmployerId { get; set; }
+      
     }
 }
