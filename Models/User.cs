3f5f174b438dc979204e37c4c7ca82b14e983edb
nuredin10 <nuredinibrahim18@gmail.com>
2022-09-22 22:18:13 +0300

    sdfg

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sera_job_portal_api.Models
+{
+    public class User
+    {
+    }
+}
