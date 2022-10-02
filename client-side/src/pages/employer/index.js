1a98cba4f6801787ca4dd7d51a39ce9f678dfe80
Naod Tadele <62254352+kronahs@users.noreply.github.com>
2022-10-02 19:11:59 +0300

    Update index.js
    Removed Comment

--- client-side/src/pages/employer/index.js
+++ client-side/src/pages/employer/index.js
@@ -131,38 +131,7 @@ const Employer = () => {
           </Grid>
         </Grid>
 
-        {/* <Grid container spacing={0}>
-          <Grid item lg={12} md={12} sm={12}>
-            <EmployerHeader/>
-          </Grid>
-          <Grid item sx={{width: '100%', height: '70vh'}}>
-            <Box style={styles.paperContainer}>
-                <Typography variant='h2' sx={{marginBottom: '3%'}}>welcome</Typography>
-                <Button variant='contained' sx={buttonStyle}>See Latest Jobs</Button>
-            </Box>      
-          </Grid>
-          <Grid container sx={{backgroundColor: 'white', p: '5%'}}>
-            <Grid item lg={6} sm={12} md={12} sx={{my: '5%'}} >
-                <img src='/employee-hero-image.jpg' style={imageStyle}></img>
-            </Grid>
-            <Grid item lg={6} sm={12} md={12} sx={{my: '5%'}}>
-                <Typography sx={{p: '5%'}} variant='body1' color="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-            </Grid>
-            <Grid item lg={6} sm={12} md={12} sx={{my: '5%'}}>
-                <Typography sx={{p: '5%'}} variant='body1' color="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-            </Grid>
-            <Grid item lg={6} sm={12} md={12} sx={{my: '5%'}}>
-                <img src='/employee-hero-image.jpg' style={imageStyle}></img>
-            </Grid>
-            <Grid item lg={6} sm={12} md={12} sx={{my: '5%'}}>
-                <img src='/employee-hero-image.jpg' style={imageStyle}></img>
-            </Grid>
-            <Grid item lg={6} sm={12} md={12} sx={{my: '5%'}}>
-                <Typography sx={{p: '5%'}} variant='body1' color="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-            </Grid>
-            
-          </Grid>
-        </Grid> */}
+        {}
       </Box>
     </>
   );
